968db25f74b61477d33a426d4b4647d4b1b85b36
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-08-14 10:32:15 +0900

    Add helper func

--- config/nvim/lua/base.lua
+++ config/nvim/lua/base.lua
@@ -5,3 +5,14 @@ opt = vim.opt
 cmd = vim.cmd
 fn = vim.fn
 api = vim.api
+
+-- map functions
+_G['map'] = function(mode, lhs, rhs, opt)
+  vim.keymap.set(mode, lhs, rhs, opt or { silent = true })
+end
+
+for _, mode in pairs({ 'n', 'v', 'i', 'o', 'c', 't', 'x', 't' }) do
+  _G[mode .. 'map'] = function(lhs, rhs, opt)
+    map(mode, lhs, rhs, opt)
+  end
+end
