ca0bb35ad7a1059a041941ec95714ca008e8051c
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-09-03 09:26:57 +0900

    Add manage session plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -134,21 +134,60 @@ require("lazy").setup({
   },
   -- セッション管理
   {
-    'xolox/vim-session',
-    dependencies = { 
-      'xolox/vim-misc'
-    },
+    "olimorris/persisted.nvim",
+    -- event = "BufReadPre",
+    -- event = "VeryLazy",
+    lazy = false,
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+    -- stylua: ignore
     keys = {
-      {'<leader>so', ':OpenSession<Space>'},
-      {'<leader>ss', ':SaveSession<Space>'},
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
     config = function()
-      g['session_directory'] = "~/.config/nvim/session"
-      g['session_autoload'] = "no"
-      g['session_autosave'] = "no"
-      g['session_command_aliases'] = 1
+      require("persisted").setup({
+        save_dir = fn.expand(fn.stdpath("data") .. "/sessions/"), -- directory where session files are saved
+        silent = false, -- silent nvim message when sourcing session file
+        use_git_branch = false, -- create session files based on the branch of the git enabled repository
+        autosave = true, -- automatically save session files when exiting Neovim
+        -- should_autosave = nil, -- function to determine if a session should be autosaved
+        should_autosave = function()
+        -- do not autosave if the alpha dashboard is the current filetype
+          if vim.bo.filetype == "alpha" then
+            return false
+          end
+          return true
+        end,
+        autoload = true, -- automatically load the session for the cwd on Neovim startup
+        on_autoload_no_session = function()
+          vim.notify("No existing session to load.")
+        end,
+        follow_cwd = true, -- change session file name to match current working directory if it changes
+        allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
+        ignored_dirs = nil, -- table of dirs that are ignored when auto-saving and auto-loading
+        telescope = { -- options for the telescope extension
+          reset_prompt_after_deletion = true, -- whether to reset prompt after session deleted
+        },
+      })
     end
   },
+  -- {
+  --   'xolox/vim-session',
+  --   dependencies = { 
+  --     'xolox/vim-misc'
+  --   },
+  --   keys = {
+  --     {'<leader>so', ':OpenSession<Space>'},
+  --     {'<leader>ss', ':SaveSession<Space>'},
+  --   },
+  --   config = function()
+  --     g['session_directory'] = "~/.config/nvim/session"
+  --     g['session_autoload'] = "no"
+  --     g['session_autosave'] = "no"
+  --     g['session_command_aliases'] = 1
+  --   end
+  -- },
   -- copilot
   {
     "zbirenbaum/copilot.lua",
@@ -355,6 +394,7 @@ require("lazy").setup({
     {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.2',
+      lazy= false,
       dependencies = { 
         'nvim-lua/plenary.nvim',
         {
@@ -371,6 +411,11 @@ require("lazy").setup({
         { '<leader>fg', ':Telescope live_grep<CR>' },
         { '<leader>fb', ':Telescope current_buffer_fuzzy_find<CR>' },
         { '<leader>fh', ':Telescope help_tags<CR>' }
+      },
+      extensions = {
+        persisted = {
+          layout_config = { width = 0.55, height = 0.55 }
+        }
       }
     },
     {
