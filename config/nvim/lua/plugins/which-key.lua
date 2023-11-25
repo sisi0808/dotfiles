86cadc5a3c99466f535566c4859582a116f4539d
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-11-25 11:06:14 +0900

    add which-key keymaps

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -6,5 +6,13 @@ return {
     o.timeout = true
     o.timeoutlen = 300
   end,
-  opts = {}
+  opts = {
+    defaults = {
+        mode = { "n", "v" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>L"] = { name = "+lazy" },
+        ["<leader>q"] = { name = "+session" },
+        ["<leader>f"] = { name = "+find" },
+      },
+  }
 }
