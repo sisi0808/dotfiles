d361c2c1861d61fe708bdcd0f5f253c528c1af9f
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2023-12-18 19:45:53 +0900

    make auto_cmd for toggle renum in active win

--- config/nvim/lua/auto_command.lua
+++ config/nvim/lua/auto_command.lua
@@ -5,8 +5,7 @@
 --   end,
 -- })
 --
--- ファイルが変更されたら自動で読み込む
--- vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+-- ファイルが変更されたら自動で読み込む vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
 --   command = "if mode() != 'c' | checktime | endif",
 --   pattern = { "*" },
 -- })
@@ -25,6 +24,27 @@ api.nvim_create_autocmd(
         end,
     }
 )
+
+-- Activeなウインドウだけrelative numberを有効にする
+local augroup = api.nvim_create_augroup("Numbertoggle", {clear=true})
+api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if api.nvim_get_mode().mode ~= "i" then
+            opt.relativenumber = true
+        end
+    end,
+})
+
+api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        opt.relativenumber = false
+        cmd("redraw")
+    end,
+})
 
 -- 今開いているバッファのファイル名をヤンク
 api.nvim_create_user_command('Nameyank', 'let @+ = expand("%")', {})
