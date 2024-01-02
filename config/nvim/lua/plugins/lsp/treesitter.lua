37325a6399f378241413a7f1a96f21b1c074c1a4
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-01-02 22:57:41 +0900

    [nvim] Add Treesitter-object.

--- config/nvim/lua/plugins/lsp/treesitter.lua
+++ config/nvim/lua/plugins/lsp/treesitter.lua
@@ -1,10 +1,13 @@
 -- sitter
 return {
-  "nvim-treesitter/nvim-treesitter",
+  'nvim-treesitter/nvim-treesitter',
   version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  event = { "BufNewFile", "BufRead" },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  build = ':TSUpdate',
+  event = { 'BufNewFile', 'BufRead' },
+  cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects'
+  },
   config = function()
     require("nvim-treesitter.configs").setup({
       highlight = { enable = true },
