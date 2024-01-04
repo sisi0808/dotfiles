b1c7b88d8d6500593b2f54cad7bca6d49e4020c6
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-01-04 21:09:35 +0900

    [nvim] Change git diff appearance.

--- config/nvim/lua/plugins/git/gitsigns.lua
+++ config/nvim/lua/plugins/git/gitsigns.lua
@@ -3,13 +3,6 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup({
-        signs = {
-          add          = { text = '+' },
-          change       = { text = '~' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~_' },
-        },
         current_line_blame = true,
     })
   end
