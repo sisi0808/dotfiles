6aeed4829c43ebab575acad4b859e7ecd2f0612e
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-01-09 09:11:55 +0900

    [nvim] Fix noice messages.

--- config/nvim/lua/plugins/ui/noice.lua
+++ config/nvim/lua/plugins/ui/noice.lua
@@ -9,10 +9,12 @@ return {
   config = function()
     require("noice").setup({
       messages = {
-        enabled = true,
+        enabled = false,
         -- view = "messages", -- default view for messages
-        view_error = "mini", -- view for errors
-        view_warn = "mini", -- view for warnings
+        -- view_error = "mini", -- view for errors
+        -- view_warn = "mini", -- view for warnings
+        -- view_error = "mini", -- view for errors
+        -- view_warn = "mini", -- view for warnings
         -- view_history = "messages", -- view for :messages
         -- view_search = "mini",
         -- excluded_filetypes = {"fern"}
