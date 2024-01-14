69cddb0ce2f7c3ea76c0d9be6175a75bc55fb83d
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-01-14 18:55:47 +0900

    [nvim] Change trouble.nvim keymaps.

--- config/nvim/lua/plugins/lsp/trouble.lua
+++ config/nvim/lua/plugins/lsp/trouble.lua
@@ -9,7 +9,12 @@ return {
 		-- refer to the configuration section below
 	},
 	keys = {
-		{ "<leader>tt", ":TroubleToggle<CR>", desc = "Trouble Toggle", silent = true },
-		{ "<leader>tr", ":TroubleReflesh<CR>", desc = "Trouble Reflesh", silent = true },
+		-- { "<leader>tt", ":TroubleToggle<CR>", desc = "Trouble Toggle", silent = true },
+		-- { "<leader>tr", ":TroubleReflesh<CR>", desc = "Trouble Reflesh", silent = true },
+    {"<leader>xx", function() require("trouble").toggle() end, desc = "Trouble Toggle", silent = true },
+    {"<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, desc = "Trouble Workspace", silent = true },
+    {"<leader>xd", function() require("trouble").toggle("document_diagnostics") end, desc = "Trouble document", silent = true },
+    {"<leader>xq", function() require("trouble").toggle("quickfix") end, desc = "Trouble quickfix", silent = true },
+    {"<leader>xl", function() require("trouble").toggle("loclist") end, desc = "Trouble loclist", silent = true },
 	},
 }
