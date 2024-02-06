f2adb623714217a37d92f58c2c35f1f7bd0b079b
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-06 21:07:17 +0900

    [nvim] Set relative num true

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -10,7 +10,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 -- 表示、見た目
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 -- vim.opt.number = true
 vim.opt.list = true
 vim.opt.listchars = { tab = ">>", trail = "-", nbsp = "+" }
