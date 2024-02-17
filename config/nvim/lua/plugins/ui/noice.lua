b4680e871b427ba1a7106285ccef7113bb0dfe20
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-17 11:06:48 +0900

    [nvim] Update noice messages.

--- config/nvim/lua/plugins/ui/noice.lua
+++ config/nvim/lua/plugins/ui/noice.lua
@@ -27,11 +27,9 @@ return {
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
-			-- you can enable a preset for easier configuration
 			presets = {
-				-- long_message_to_split = true, -- long messages will be sent to a split
-				-- inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
+				long_message_to_split = true, -- long messages will be sent to a split
+				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
 		})
 	end,
