42a0a83d0953e29765dc4e60f61c8034d6caa8a0
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-19 07:27:21 +0900

    [nvim] Add treesitter-textobj plugins.

--- config/nvim/lua/plugins/lsp/treesitter.lua
+++ config/nvim/lua/plugins/lsp/treesitter.lua
@@ -6,7 +6,36 @@ return {
 	event = { "BufNewFile", "BufRead" },
 	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			config = function()
+				local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+				local configs = require("nvim-treesitter.configs")
+				require("nvim-treesitter.configs").setup({
+					textobjects = {
+						select = {
+							enable = true,
+
+							lookahead = true,
+
+							keymaps = {
+								["af"] = "@function.outer",
+								["if"] = "@function.inner",
+								["ac"] = "@class.outer",
+								["ic"] = "@class.inner",
+							},
+							selection_modes = {
+								["@parameter.outer"] = "v", -- charwise
+								["@function.outer"] = "V", -- linewise
+								["@class.outer"] = "<c-v>", -- blockwise
+							},
+							include_surrounding_whitespace = true,
+						},
+					},
+				})
+			end,
+		},
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 	config = function()
 		require("nvim-treesitter.configs").setup({
