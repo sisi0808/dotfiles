6a7552e1b7917c028490ef6a4232b16520da4220
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-21 06:21:49 +0900

    [nvim] Add skk dictionary.

--- config/nvim/lua/plugins/lsp/skkeleton.lua
+++ config/nvim/lua/plugins/lsp/skkeleton.lua
@@ -11,11 +11,20 @@ return {
   },
   lazy = false,
 	config = function()
+    local dicPath = "~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries"
 		vim.fn["skkeleton#config"]({
 			eggLikeNewline = true,
 			registerConvertResult = true,
 			globalDictionaries = {
 				"~/.skk/SKK-JISYO.L",
+        dicPath .. "SKK-JISYO.assoc",
+        dicPath .. "SKK-JISYO.geo",
+        dicPath .. "SKK-JISYO.jinmei",
+        dicPath .. "SKK-JISYO.L",
+        dicPath .. "SKK-JISYO.law",
+        dicPath .. "SKK-JISYO.propernoun",
+        dicPath .. "SKK-JISYO.station",
+        dicPath .. "skk-jisyo.utf8",
 			},
 		})
 	end,
