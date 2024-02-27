922d03270505dabe2975062283779b4f73c6085c
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-27 12:28:38 +0900

    [skk] Fix typo

--- config/nvim/lua/plugins/lsp/skkeleton.lua
+++ config/nvim/lua/plugins/lsp/skkeleton.lua
@@ -11,7 +11,7 @@ return {
   },
   lazy = false,
 	config = function()
-    local dicPath = "~/.skk"
+    local dicPath = "~/.skk/"
 		vim.fn["skkeleton#config"]({
 			eggLikeNewline = true,
 			registerConvertResult = true,
@@ -20,7 +20,6 @@ return {
         dicPath .. "SKK-JISYO.assoc",
         dicPath .. "SKK-JISYO.geo",
         dicPath .. "SKK-JISYO.jinmei",
-        dicPath .. "SKK-JISYO.L",
         dicPath .. "SKK-JISYO.law",
         dicPath .. "SKK-JISYO.propernoun",
         dicPath .. "SKK-JISYO.station",
