00a6a29a8d0006137b0ccc54ff554d68e0f29e91
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-02-28 06:29:19 +0900

    [skk] Fix skkeleton keymap

--- config/nvim/lua/plugins/lsp/skkeleton.lua
+++ config/nvim/lua/plugins/lsp/skkeleton.lua
@@ -1,36 +1,42 @@
 return {
 	"vim-skk/skkeleton",
 	dependencies = {
-    "vim-denops/denops.vim",
-    {
-      "delphinus/skkeleton_indicator.nvim",
-      config = function()
-        require'skkeleton_indicator'.setup{}
-      end
-    }
-  },
-  lazy = false,
+		"vim-denops/denops.vim",
+		{
+			"delphinus/skkeleton_indicator.nvim",
+			config = function()
+				require("skkeleton_indicator").setup({})
+			end,
+		},
+	},
+	lazy = false,
 	config = function()
-    local dicPath = "~/.skk/"
+		local dicPath = "~/.skk/"
 		vim.fn["skkeleton#config"]({
 			eggLikeNewline = true,
 			registerConvertResult = true,
 			globalDictionaries = {
-        dicPath .. "SKK-JISYO.L",
-        dicPath .. "SKK-JISYO.assoc",
-        dicPath .. "SKK-JISYO.geo",
-        dicPath .. "SKK-JISYO.jinmei",
-        dicPath .. "SKK-JISYO.law",
-        dicPath .. "SKK-JISYO.propernoun",
-        dicPath .. "SKK-JISYO.station",
-        dicPath .. "skk-jisyo.utf8",
+				dicPath .. "SKK-JISYO.L",
+				dicPath .. "SKK-JISYO.assoc",
+				dicPath .. "SKK-JISYO.geo",
+				dicPath .. "SKK-JISYO.jinmei",
+				dicPath .. "SKK-JISYO.law",
+				dicPath .. "SKK-JISYO.propernoun",
+				dicPath .. "SKK-JISYO.station",
 			},
-      userDictionary = "~/.skk/.skkeleton"
+			userDictionary = dicPath .. ".skkeleton",
+			kanaTable = "azik",
+			globalKanaTableFiles = {
+				dicPath .. "azik_us.rule",
+			},
 		})
+		vim.fn["skkeleton#register_keymap"]("input", ";", "henkanPoint")
+		vim.fn["skkeleton#register_keymap"]("henkan", "X", false)
+		vim.fn["skkeleton#register_keymap"]("henkan", "<", "purgeCandidate")
 	end,
 	keys = {
 		{ "<C-j>", "<Plug>(skkeleton-toggle)", mode = "i" },
 		{ "<C-j>", "<Plug>(skkeleton-toggle)", mode = "c" },
-    { "<C-j>", "<Plug>(skkeleton-toggle)", mode = "t" },
+		{ "<C-j>", "<Plug>(skkeleton-toggle)", mode = "t" }
 	},
 }
