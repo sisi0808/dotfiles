84407de27970d6f7b2a9a28f47962ea2ae1d88b6
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-03-06 07:46:17 +0900

    [nvim] Ser relativenumber

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -9,8 +9,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 -- 表示、見た目
+vim.opt.number = true
 vim.opt.relativenumber = true
--- vim.opt.number = true
 vim.opt.list = true
 vim.opt.listchars = { tab = ">>", trail = "-", nbsp = "+" }
 vim.opt.title = true
