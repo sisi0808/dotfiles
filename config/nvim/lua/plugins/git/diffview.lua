27f372a2b187129ebe93f9a9181aceb57707487b
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-03-20 08:17:10 +0900

    Revert "[nvim] Add diffview plug."
    This reverts commit 2fcc152d88dec1cdb19f576b00f5dc80ac5b9010.

--- config/nvim/lua/plugins/git/diffview.lua
+++
@@ -1,8 +0,0 @@
--- Git差分のViewを提供
-return {
-	"sindrets/diffview.nvim",
-	keys = {
-		{ "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "GitDiffOpen", silent = true },
-		{ "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "GitDiffClose", silent = true },
-	},
-}
