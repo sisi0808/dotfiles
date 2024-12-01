f415b51a67c5e6dc474425934a7c2a1f48d9466a
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2024-12-01 09:13:13 +0900

    [nvim] Add nvim-java

--- config/nvim/lua/plugins/lsp/nvim-java.lua
+++ config/nvim/lua/plugins/lsp/nvim-java.lua
@@ -1,26 +1,49 @@
 -- Java development plugin for Neovim
 return {
-	--   'nvim-java/nvim-java',
-	--   dependencies = {
-	--     'nvim-java/lua-async-await',
-	--     'nvim-java/nvim-java-core',
-	--     'nvim-java/nvim-java-test',
-	--     'nvim-java/nvim-java-dap',
-	--     'MunifTanjim/nui.nvim',
-	--     'neovim/nvim-lspconfig',
-	--     'mfussenegger/nvim-dap',
-	--     {
-	--       'williamboman/mason.nvim',
-	--       opts = {
-	--         registries = {
-	--           'github:nvim-java/mason-registry',
-	--           'github:mason-org/mason-registry',
-	--         },
-	--       },
-	--     }
-	--   },
-	--   config = function()
-	--     require('java').setup()
-	--     require('lspconfig').jdtls.setup({})
-	--   end
+	"nvim-java/nvim-java",
+	dependencies = {
+		"nvim-java/lua-async-await",
+		"nvim-java/nvim-java-core",
+		"nvim-java/nvim-java-test",
+		"nvim-java/nvim-java-dap",
+		"MunifTanjim/nui.nvim",
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"rcarriga/nvim-dap-ui",
+		{
+			"williamboman/mason.nvim",
+			opts = {
+				registries = {
+					"github:nvim-java/mason-registry",
+					"github:mason-org/mason-registry",
+				},
+			},
+		},
+	},
+	config = function()
+		require("java").setup()
+		require("lspconfig").jdtls.setup({
+			settings = {
+				java = {
+					configuraiton = {
+						runtimes = {
+							{
+								name = "OpenJDK-21",
+								path = "/opt/homebrew/opt/openjdk@21/bin/java",
+								default = true,
+							},
+						},
+					},
+					-- Code Stryles
+					-- format = {
+					--   enabled = true,
+					--   settings = {
+					--     url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+					--     profile = "co",
+					--   },
+					-- },
+				},
+			},
+		})
+	end,
 }
