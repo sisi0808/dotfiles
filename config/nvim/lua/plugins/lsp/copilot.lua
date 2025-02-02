efbb22990611b002f5080644c5e9b19f42373951
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2025-02-03 08:50:24 +0900

    [nvim] Add copilot-caht

--- config/nvim/lua/plugins/lsp/copilot.lua
+++ config/nvim/lua/plugins/lsp/copilot.lua
@@ -1,25 +1,34 @@
 -- copilot
 return {
-	"zbirenbaum/copilot.lua",
-	-- dependencies = { "gptlang/CopilotChat.nvim" },
-	cmd = "Copilot",
-	event = "InsertEnter",
-	lazy = false,
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<C-k>",
-					accept_word = false,
-					accept_line = false,
-					-- next = "<C-o>",
-					-- prev = "<C-i>",
-					dismiss = "<C-]>",
-				},
+	{
+		"zbirenbaum/copilot.lua",
+		dependencies = {
+			"CopilotC-Nvim/CopilotChat.nvim",
+			dependencies = {
+				{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 			},
-		})
-	end,
+			build = "make tiktoken", -- Only on MacOS or Linux
+			opts = {},
+		},
+		cmd = "Copilot",
+		event = "InsertEnter",
+		lazy = false,
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept = "<C-k>",
+						accept_word = false,
+						accept_line = false,
+						-- next = "<C-o>",
+						-- prev = "<C-i>",
+						dismiss = "<C-]>",
+					},
+				},
+			})
+		end,
+	},
 }
