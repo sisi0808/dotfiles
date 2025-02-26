99d07e6976c0d7df28e7eb9060cf9f1d5cc952f9
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2025-02-26 21:59:39 +0900

    :sparkles:Add gitsigns keymap

--- config/nvim/lua/plugins/git/gitsigns.lua
+++ config/nvim/lua/plugins/git/gitsigns.lua
@@ -1,13 +1,18 @@
 -- Gitの差分表示
 return {
 	"lewis6991/gitsigns.nvim",
-  lazy=false,
+	lazy = false,
 	config = function()
 		require("gitsigns").setup({
 			current_line_blame = true,
 		})
 	end,
-  keys = {
-    { "<leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "GitFileDiff", silent = true },
-  }
+	keys = {
+		{ "<leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "GitFileDiff", silent = true },
+		{ "<leader>gb", "<cmd>Gitsigns blame<cr>", desc = "GitBlame", silent = true },
+		{ "<leader>gq", "<cmd>Gitsigns setloclist<cr>", desc = "GitDiffQf", silent = true },
+		{ "<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "GitDelete", silent = true },
+		{ "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", desc = "GitStageHunk", silent = true },
+		{ "<leader>gS", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "GitUnStageHunk", silent = true },
+	},
 }
