c55cb9fdb4199be113083df182ef874b7772984e
sisi0808 <52097096+sisi0808@users.noreply.github.com>
2025-03-04 19:03:44 +0900

    :sparkles:Add diffview keymaps.

--- config/nvim/lua/plugins/git/diffview.lua
+++ config/nvim/lua/plugins/git/diffview.lua
@@ -3,10 +3,17 @@ return {
 	"sindrets/diffview.nvim",
 	lazy = false,
 	config = function()
-		require("diffview").setup({})
+		require("diffview").setup({
+			keymaps = {
+				file_panel = {
+					{ "n", "ss", require("diffview.actions").goto_file_split, desc = "Open the file in a new split" },
+				},
+			},
+		})
 	end,
-  keys = {
-    { "<leader>gp", ":DiffviewOpen develop<CR>", desc = "Git PR review with develop", silent = true },
-    { "<leader>gh", ":DiffviewFileHistory %<CR>", desc = "Git file history", silent = true },
-  },
+	keys = {
+		{ "<leader>gp", ":DiffviewOpen develop<CR>", desc = "Git PR review with develop", silent = true },
+		{ "<leader>gh", ":DiffviewFileHistory %<CR>", desc = "Git file history", silent = true },
+		{ "<leader>ge", ":DiffviewFiletoggleFiles<CR>", desc = "Git toggle files", silent = true },
+	},
 }
